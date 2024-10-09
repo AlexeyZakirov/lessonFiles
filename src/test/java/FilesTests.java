a1018fea4cf5c416c82991d590fda86340068226
AlexeyZakirov <tinwhipcf@gmail.com>
2024-10-09 22:42:57 +0900

    add tests in files

--- src/test/java/FilesTests.java
+++ src/test/java/FilesTests.java
@@ -1,7 +1,6 @@
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -13,6 +12,8 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FilesTests {
     private final String pathToZipFile = "src/test/resources/filesLessons.zip";
     private final String pathToPdfFile = "fileslessons/pdfExample.pdf";
@@ -22,58 +23,55 @@ public class FilesTests {
     @Tag("SMOKE")
     @DisplayName("PDF файл должен содержать ожидаемый текст")
     @Test
-    public void readPdfInZipTest() throws Exception{
-        try(ZipFile zipFile = new ZipFile(pathToZipFile)){
+    public void readPdfInZipTest() throws Exception {
+        try (ZipFile zipFile = new ZipFile(pathToZipFile)) {
             ZipEntry entry = zipFile.getEntry(pathToPdfFile);
             PDF pdf = new PDF(zipFile.getInputStream(entry));
 
-            Assertions.assertEquals(2, pdf.numberOfPages);
-            Assertions.assertTrue(pdf.text.contains("Lorem ipsum dolor sit amet, consectetuer adipiscing elit"));
+            assertThat(pdf.numberOfPages).isEqualTo(2);
+            assertThat(pdf.text).contains("Lorem ipsum dolor sit amet, consectetuer adipiscing elit");
         }
     }
 
     @Tag("SMOKE")
     @DisplayName("XLSX файл должен содержать ожидаемый текст в ячейке после внесения изменений")
     @Test
-    public void editAndReadXlsxInZipTest() throws Exception{
-        try(ZipFile zipFile = new ZipFile(pathToZipFile)){
+    public void editAndReadXlsxInZipTest() throws Exception {
+        try (ZipFile zipFile = new ZipFile(pathToZipFile)) {
             ZipEntry entry = zipFile.getEntry(pathToXlsxFile);
             XLS xls = new XLS(zipFile.getInputStream(entry));
 
             xls.excel.getSheetAt(1).getRow(3).getCell(2).setCellValue("Activities!!!!!");
             String resultCellValue = xls.excel.getSheetAt(1).getRow(3).getCell(2).getStringCellValue();
 
-            Assertions.assertEquals("Activities!!!!!", resultCellValue);
+            assertThat(resultCellValue).isEqualTo("Activities!!!!!");
         }
     }
 
     @Tag("SMOKE")
     @DisplayName("Значения строк CSV файла должны соответствовать ожидаемым значениям")
     @Test
-    public void readCsvInZipTest() throws Exception{
-        try (ZipFile zipFile = new ZipFile(pathToZipFile)){
+    public void readCsvInZipTest() throws Exception {
+        try (ZipFile zipFile = new ZipFile(pathToZipFile)) {
             ZipEntry entry = zipFile.getEntry(pathToCsvFile);
 
-        try(InputStream inputStream = zipFile.getInputStream(entry);
-            CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            try (InputStream inputStream = zipFile.getInputStream(entry);
+                 CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
 
-            List<String[]> data = reader.readAll();
+                List<String[]> data = reader.readAll();
 
-            Assertions.assertEquals(3, data.size());
-            Assertions.assertArrayEquals(
-                    new String[] {"Name", "Job Title", "Address", "State", "City"},
-                    data.get(0)
-            );
-            Assertions.assertArrayEquals(
-                    new String[] {"John Doe", "Designer", "325 Pine Street", "", "Seattle"},
-                    data.get(1)
-            );
-            Assertions.assertArrayEquals(
-                    new String[] {"Edward Green", "Developer", "110 Pike Street", "WA", "Seattle"},
-                    data.get(2)
-            );
+                assertThat(data.size()).isEqualTo(3);
 
-        }
+                assertThat(data.get(0)).isEqualTo(new String[]{
+                        "Name", "Job Title", "Address", "State", "City"
+                });
+                assertThat(data.get(1)).isEqualTo(new String[]{
+                        "John Doe", "Designer", "325 Pine Street", "", "Seattle"
+                });
+                assertThat(data.get(2)).isEqualTo(new String[]{
+                        "Edward Green", "Developer", "110 Pike Street", "WA", "Seattle"
+                });
+            }
         }
     }
 }
--- src/test/java/JsonTests.java
+++ src/test/java/JsonTests.java
@@ -1,11 +1,12 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jsonObjects.Student;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class JsonTests {
     private final String pathToJsonFile = "src/test/resources/student.json";
@@ -18,10 +19,12 @@ public class JsonTests {
         File file = new File(pathToJsonFile);
         Student student = objectMapper.readValue(file, Student.class);
 
-        Assertions.assertEquals("John", student.getFirstName());
-        Assertions.assertEquals("Snow", student.getLastName());
-        Assertions.assertEquals(15, student.getAge());
-        Assertions.assertEquals(false, student.isHasScholarship());
-        Assertions.assertArrayEquals(new String[]{"Math", "Chemistry", "History"}, student.getFavoriteSubjects());
+        assertThat(student.getFirstName()).isEqualTo("John");
+        assertThat(student.getLastName()).isEqualTo("Snow");
+        assertThat(student.getAge()).isEqualTo(15);
+        assertThat(student.isHasScholarship()).isEqualTo(false);
+        assertThat(student.getFavoriteSubjects()).isEqualTo(new String[]{
+                "Math", "Chemistry", "History"
+        });
     }
 }
